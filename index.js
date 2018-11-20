e4606b4e68672c3845e46c681cbc50281e4ce35b
Santiago Demierre <santidemierre@gmail.com>
2018-11-20 11:26:04 -0300

    curso finalizado de bootstrap. Primer y unico commit

---
+++ index.js
@@ -0,0 +1,4 @@
+$(function () { //Esta funcion se va a cargar cuando se carguen todos los demás scripts.
+    $('[data-toggle="tooltip"]').tooltip()
+    // Esto es un pluggin de jQuery que agrega bootstrap por nosotros.
+})
