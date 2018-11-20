baaac6472a3302ac71309bf20d10649e5a7cf0bb
Santiago Demierre <santidemierre@gmail.com>
2018-11-20 12:05:13 -0300

    Arraglando error del menú para dispositivos pequeños

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 $(function () { //Esta funcion se va a cargar cuando se carguen todos los demás scripts.
     $('[data-toggle="tooltip"]').tooltip()
     // Esto es un pluggin de jQuery que agrega bootstrap por nosotros.
-})
+});
+
+$(‘#miGrilla’).bootstrap(‘grid’);
